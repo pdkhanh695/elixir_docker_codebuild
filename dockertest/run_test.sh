1fb1731619c6f63f44353c4c4037c47c9d7859a7
khanhphan <pdkhanh95@gmail.com>
2020-08-02 18:03:00 +0700

    ad

---
+++ dockertest/run_test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# mix deps.get
+
+# mix ecto.create
+# mix ecto.migrate
+
+# mix test
+
+# mix phx.server
+
+# echo "## checking format"
+# mix format --check-formatted
+
+# echo "## run credo for linting"
+# mix credo --strict
+
+# echo "## running tests"
+# mix test --trace
+
+# echo "## run coverage"
+# mix coveralls.html
+
+# echo "## run dialyzer"
+# mix dialyzer
+
+# echo "## run sobelow security check"
+# mix sobelow --ignore Config.HTTPS,Config.CSRF,Config.Headers --private --verbose --exit
+
+echo "We are in test"
+mix deps.get
+
+mix test
+
+mix test --cover
+
+# echo "## run dialyzer"
+mix dialyzer
+
+# echo "## run coverage"
+mix coveralls.html
+
+# echo "## checking format"
+mix format --check-formatted
+
+# echo "## run credo for linting"
+mix credo --strict
+
+# echo "## running tests"
+mix test --trace
